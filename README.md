36df68dc06831d8542d3de79cc688180798f4c57
niaeashes <nia.eashes@gmail.com>
2015-10-25 02:25:27 +0900

    support to setup custom post type supports via method chaing

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ cd wp-content/themes/your_theme
 git clone git@github.com:niaeashes/paper.git modules
 echo 'require get_template_directory() . "/modules/include.php";' > functions.php
 ```
+
+## Create custom post type
+
+Define custom post type using method chain.
+
+```php
+require get_template_directory() . "/modules/include.php";
+(new Paper_Post_Type('custom'))
+  ->support_thumbnail()
+  ->support_custom_field();
+```
+
+Note: 'title' and 'editor' is default supported.
--- class-paper-post-type.php
+++ class-paper-post-type.php
@@ -2,41 +2,61 @@
 
 class Paper_Post_Type
 {
-	public function Paper_Post_Type( $post_type, $options )
+	public function Paper_Post_Type( $post_type, $options = array() )
 	{
 		$this->post_type = $post_type;
 		$this->options = $options;
-		$this->supports = $options['supports'];
+		if ( array_key_exists('supports', $options) ) {
+			$this->supports = array_merge($options['supports'], $this->supports );
+		}
 		add_action( 'init', array( $this, 'init' ), 0 );
 	}
 
 	private $post_type;
-	private $options;
-	private $supports;
+	private $options = array();
+	private $supports = array( 'title', 'editor' );
 
 	public function init()
 	{
 		$labels = array(
-			'singular_name' => self::singularize(ucfirst($this->post_type))
+			'singular_name' => Paper_Inflector::singularize(ucfirst($this->post_type))
 		);
 		$options = array_merge(array(
-			'label' => self::pluralize(ucfirst($this->post_type)),
+			'label' => Paper_Inflector::pluralize(ucfirst($this->post_type)),
 			'labels' => $labels,
 			'rewrite' => array( 'slug' => $this->post_type ),
 			'public' => true,
 			'has_archive' => true
 		), $this->options);
-		$options['supports'] = array_merge($this->supports, array( 'title', 'editor' ) );
+		$options['supports'] = $this->supports;
 		register_post_type( $this->post_type, $options );
 	}
 
-	static public function pluralize( $name )
+	static private $_allowed_supports = array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom_fields', 'comments', 'revisions', 'page-attributes', 'post-formats');
+
+	public function __call($name, $arguments)
 	{
-		return $name.'s';
+		if ( preg_match( '/^support_(' . str_replace('-', '_', implode(self::$_allowed_supports, '|')) . ')$/', $name, $matches ) ) {
+			return $this->add_support( str_replace('_', '-', $matches[1]) );
+		}
 	}
 
-	static public function singularize( $name )
+	/**
+	 * Add support to this post type.
+	 *
+	 * You can use alias name is wraped by __call magic method.
+	 *
+	 * @alias add_support('title') => support_title()
+	 * @alias add_support('custom-field') => support_custom_field()
+	 * @param string in 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes', 'post-formats'.
+	 */
+	public function add_support( $value )
 	{
-		return preg_replace( '/s$/i', '', $name.'s' );
+		if ( in_array( $value, self::$_allowed_supports ) ) {
+			array_push( $this->supports, $value );
+		} else {
+			// TODO: Warning
+		}
+		return $this;
 	}
 }
