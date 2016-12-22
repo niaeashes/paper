028fa4811a6b8c24eae159e462410cf187897351
niaeashes <nia.eashes@gmail.com>
2016-12-22 13:41:27 +0900

    Update Paper_Script arguments

--- class/class-paper-script.php
+++ class/class-paper-script.php
@@ -4,19 +4,21 @@ class Paper_Script
 {
   private $name = "";
   private $path = "";
-  private $options = [];
+  private $deps = [];
 
-  public function Paper_Script( $name, $path, $options = [] )
+  public function Paper_Script( $name, $path, array $deps = array(), $ver = false, $in_footer = false )
   {
 		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
 
     $this->name = $name;
     $this->path = $path;
-    $this->options = $options;
+    $this->deps = $deps;
+    $this->ver = $ver;
+    $this->in_footer = $in_footer;
   }
 
   public function wp_enqueue_scripts()
   {
-  	wp_enqueue_script( $this->name, $this->path);
+  	wp_enqueue_script( $this->name, $this->path, $this->deps, $this->ver, $this->in_footer);
   }
 }
